e7343ce100c80bd13735433554d0dc00d983e0f0
kurosutomohiro <kurosu.66@gmail.com>
2022-05-05 18:19:19 +0900

    rubocop修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -13,14 +13,14 @@ def main
 
   if file_names.any? && options[:l]
     format_l_option(file_names)
-  elsif file_names.any? && options.empty?
+  elsif file_names.any? # && options.empty?
     format_normal(file_names)
   # ファイル指定なし、lオプション
   elsif file_names.empty? && options[:l]
     input = $stdin.readlines
     format_stdin_l_option(input)
   # ファイル指定なし、オプションなし
-  elsif file_names.empty? && options.empty?
+  elsif file_names.empty? # && options.empty?
     input = $stdin.readlines
     format_stdin(input)
   end
@@ -30,13 +30,14 @@ def format_normal(file_names)
   total_line_count = file_names.sum { |file_name| File.read(file_name).count("\n") }.to_s.rjust(4)
   total_word_cout = file_names.sum { |file_name| File.read(file_name).split(/\s+/).count }.to_s.rjust(4)
   total_byte_count = file_names.sum { |file_name| File.size(file_name) }.to_s.rjust(4)
-  
+
   items =
-    file_names.map do |file_name| {
-      line_count: File.read(file_name).count("\n").to_s.rjust(4),
-      word_count: File.read(file_name).split(/\s+/).count.to_s.rjust(4),
-      byte_count: File.size(file_name).to_s.rjust(4),
-      file: file_name
+    file_names.map do |file_name|
+      {
+        line_count: File.read(file_name).count("\n").to_s.rjust(4),
+        word_count: File.read(file_name).split(/\s+/).count.to_s.rjust(4),
+        byte_count: File.size(file_name).to_s.rjust(4),
+        file: file_name
       }
     end
   display_normal(items, total_line_count, total_word_cout, total_byte_count)
@@ -46,19 +47,20 @@ def display_normal(items, total_line_count, total_word_cout, total_byte_count)
   items.each do |item|
     puts "    #{item[:line_count]}    #{item[:word_count]}    #{item[:byte_count]} #{item[:file]}"
   end
-  if items.size >= 2
-    puts "    #{total_line_count}    #{total_word_cout}    #{total_byte_count} total"
-  end
+  return unless items.size >= 2
+
+  puts "    #{total_line_count}    #{total_word_cout}    #{total_byte_count} total"
 end
 
 def format_l_option(file_names)
   total_line_count = file_names.sum { |file_name| File.read(file_name).count("\n") }.to_s.rjust(4)
   items =
-    file_names.map do |file_name| {
-      line_count: File.open(file_name).read.count("\n").to_s.rjust(4),
-      file: file_name
-    }
-  end
+    file_names.map do |file_name|
+      {
+        line_count: File.open(file_name).read.count("\n").to_s.rjust(4),
+        file: file_name
+      }
+    end
   display_l_option(items, total_line_count)
 end
 
@@ -66,9 +68,9 @@ def display_l_option(items, total_line_count)
   items.each do |item|
     puts "    #{item[:line_count]} #{item[:file]}"
   end
-  if items.size >= 2
-    puts "    #{total_line_count} total"
-  end
+  return unless items.size >= 2
+
+  puts "    #{total_line_count} total"
 end
 
 def format_stdin_l_option(input)
@@ -83,7 +85,7 @@ end
 def format_stdin(input)
   line_count = input.size
   word_count = input.each.sum { |v| v.split(' ').size }
-  byte_count = input.each.sum { |v| v.bytesize }
+  byte_count = input.each.sum(&:bytesize)
   display_stdin(line_count, word_count, byte_count)
 end
 
