3c4203265995d079adcc9aa8ec9b2b9e92f4ddf1
kurosutomohiro <kurosu.66@gmail.com>
2022-05-27 08:55:25 +0900

    幅の調整用スペースを定数に修正、同じ処理を1箇所にまとめる修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -5,61 +5,79 @@ require 'optparse'
 require 'debug'
 require 'readline'
 
+AJUST_SPACE = 4
+
 def main
   options = {}
   opt = OptionParser.new
   opt.on('-l') { |v| options[:l] = v }
   file_names = opt.parse!(ARGV)
 
-  if file_names.any? && options[:l]
-    format_l_option(file_names)
-  elsif file_names.any?
-    format(file_names)
-  elsif file_names.empty? && options[:l]
-    input = $stdin.readlines
-    format_stdin_l_option(input)
-  elsif file_names.empty?
-    input = $stdin.readlines
-    format_stdin(input)
+  if file_names.empty?
+    wc_from_stdin(options)
+  else
+    wc_with_file_name(file_names, options)
   end
 end
 
-def format(file_names)
-  total_line_count = file_names.sum { |file_name| File.read(file_name).count("\n") }.to_s.rjust(4)
-  total_word_cout = file_names.sum { |file_name| File.read(file_name).split(' ').size }.to_s.rjust(4)
-  total_byte_count = file_names.sum { |file_name| File.size(file_name) }.to_s.rjust(4)
+# 標準入力用のメソッド
+def wc_from_stdin(options)
+  input = $stdin.readlines
+  line_count = input.size
 
-  items =
-    file_names.map do |file_name|
-      {
-        line_count: File.read(file_name).count("\n").to_s.rjust(4),
-        word_count: File.read(file_name).split(' ').size.to_s.rjust(4),
-        byte_count: File.size(file_name).to_s.rjust(4),
-        file: file_name
-      }
-    end
-  display_normal(items, total_line_count, total_word_cout, total_byte_count)
-end
-
-def display_normal(items, total_line_count, total_word_cout, total_byte_count)
-  items.each do |item|
-    puts "    #{item[:line_count]}    #{item[:word_count]}    #{item[:byte_count]} #{item[:file]}"
+  if options[:l]
+    display_stdin_l_option(line_count)
+  else
+    word_count = input.each.sum { |v| v.split(' ').size }
+    byte_count = input.each.sum(&:bytesize)
+    display_stdin(line_count, word_count, byte_count) 
   end
-  return unless items.size >= 2
-
-  puts "    #{total_line_count}    #{total_word_cout}    #{total_byte_count} total"
 end
 
-def format_l_option(file_names)
-  total_line_count = file_names.sum { |file_name| File.read(file_name).count("\n") }.to_s.rjust(4)
+# 標準入力でないメソッド
+def wc_with_file_name(file_names, options)
+  texts = []
+  file_names.each do |v|
+    texts << File.read(v)
+  end
+
   items =
-    file_names.map do |file_name|
+    texts.map.with_index do |text, i|
       {
-        line_count: File.open(file_name).read.count("\n").to_s.rjust(4),
-        file: file_name
+      line_counts: text.count("\n"),
+      word_counts: text.split(' ').size,
+      byte_counts: text.size,
+      file_names: file_names[i],
       }
     end
-  display_l_option(items, total_line_count)
+
+  total_line_count = 0
+  total_word_count = 0
+  total_byte_count = 0
+  items.each do |v|
+    total_line_count += v[:line_counts]
+    total_word_count += v[:word_counts]
+    total_byte_count += v[:byte_counts]
+  end
+
+  if options[:l]
+    display_l_option(items, total_line_count)
+  else
+    display_normal(items, file_names, total_line_count, total_word_count, total_byte_count)
+  end
+end
+
+def calc_line_count(text, file_names)
+  line_count = text.count("\n")
+end
+
+def display_normal(items, file_names, total_line_count, total_word_count, total_byte_count)
+  items.each.with_index do |v, i|
+  puts "    #{v[:line_counts].to_s.rjust(AJUST_SPACE)}    #{v[:word_counts].to_s.rjust(AJUST_SPACE)}    #{v[:byte_counts].to_s.rjust(AJUST_SPACE)} #{v[:file_names]}"
+  end
+  if file_names.size >= 2
+    puts "    #{total_line_count.to_s.rjust(AJUST_SPACE)}      #{total_word_count}     #{total_byte_count} total"
+  end
 end
 
 def display_l_option(items, total_line_count)
@@ -71,20 +89,8 @@ def display_l_option(items, total_line_count)
   puts "    #{total_line_count} total"
 end
 
-def format_stdin_l_option(input)
-  line_count = input.size
-  display_stdin_l_option(line_count)
-end
-
 def display_stdin_l_option(line_count)
   puts "       #{line_count} "
-end
-
-def format_stdin(input)
-  line_count = input.size
-  word_count = input.each.sum { |v| v.split(' ').size }
-  byte_count = input.each.sum(&:bytesize)
-  display_stdin(line_count, word_count, byte_count)
 end
 
 def display_stdin(line_count, word_count, byte_count)
