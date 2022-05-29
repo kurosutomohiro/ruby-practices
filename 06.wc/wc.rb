85c3a6b1dbd20228c10d203161bf9ef27667a533
kurosutomohiro <kurosu.66@gmail.com>
2022-05-29 09:01:33 +0900

    不要箇所削除、rubocop指摘修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -13,14 +13,18 @@ def main
   opt.on('-l') { |v| options[:l] = v }
   file_names = opt.parse!(ARGV)
 
+  texts = []
+  file_names.each do |v|
+    texts << File.read(v)
+  end
+
   if file_names.empty?
     wc_from_stdin(options)
   else
-    wc_with_file_name(file_names, options)
+    wc_with_file_name(file_names, options, texts)
   end
 end
 
-# 標準入力用のメソッド
 def wc_from_stdin(options)
   input = $stdin.readlines
   line_count = input.size
@@ -30,59 +34,49 @@ def wc_from_stdin(options)
   else
     word_count = input.each.sum { |v| v.split(' ').size }
     byte_count = input.each.sum(&:bytesize)
-    display_stdin(line_count, word_count, byte_count) 
+    display_stdin(line_count, word_count, byte_count)
   end
 end
 
-# 標準入力でないメソッド
-def wc_with_file_name(file_names, options)
-  texts = []
-  file_names.each do |v|
-    texts << File.read(v)
-  end
-
+def wc_with_file_name(file_names, options, texts)
   items =
     texts.map.with_index do |text, i|
       {
-      line_counts: text.count("\n"),
-      word_counts: text.split(' ').size,
-      byte_counts: text.size,
-      file_names: file_names[i],
+        line_counts: text.count("\n"),
+        word_counts: text.split(' ').size,
+        byte_counts: text.size,
+        file_names: file_names[i]
       }
     end
 
   total_line_count = 0
   total_word_count = 0
   total_byte_count = 0
-  items.each do |v|
-    total_line_count += v[:line_counts]
-    total_word_count += v[:word_counts]
-    total_byte_count += v[:byte_counts]
+  items.each do |item|
+    total_line_count += item[:line_counts]
+    total_word_count += item[:word_counts]
+    total_byte_count += item[:byte_counts]
   end
 
   if options[:l]
     display_l_option(items, total_line_count)
   else
-    display_normal(items, file_names, total_line_count, total_word_count, total_byte_count)
+    display_normal(items, total_line_count, total_word_count, total_byte_count)
   end
 end
 
-def calc_line_count(text, file_names)
-  line_count = text.count("\n")
-end
+def display_normal(items, total_line_count, total_word_count, total_byte_count)
+  items.each do |item|
+    puts "    #{item[:line_counts].to_s.rjust(AJUST_SPACE)}    #{item[:word_counts].to_s.rjust(AJUST_SPACE)}    #{item[:byte_counts].to_s.rjust(AJUST_SPACE)} #{item[:file_names]}"
+  end
+  return unless items.size >= 2
 
-def display_normal(items, file_names, total_line_count, total_word_count, total_byte_count)
-  items.each.with_index do |v, i|
-  puts "    #{v[:line_counts].to_s.rjust(AJUST_SPACE)}    #{v[:word_counts].to_s.rjust(AJUST_SPACE)}    #{v[:byte_counts].to_s.rjust(AJUST_SPACE)} #{v[:file_names]}"
-  end
-  if file_names.size >= 2
-    puts "    #{total_line_count.to_s.rjust(AJUST_SPACE)}      #{total_word_count}     #{total_byte_count} total"
-  end
+  puts "    #{total_line_count.to_s.rjust(AJUST_SPACE)}    #{total_word_count.to_s.rjust(AJUST_SPACE)}    #{total_byte_count.to_s.rjust(AJUST_SPACE)} total"
 end
 
 def display_l_option(items, total_line_count)
   items.each do |item|
-    puts "    #{item[:line_count]} #{item[:file]}"
+    puts "    #{item[:line_counts].to_s.rjust(AJUST_SPACE)} #{item[:file_names]}"
   end
   return unless items.size >= 2
 
