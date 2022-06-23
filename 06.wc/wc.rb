1dc1e29f14a1374bc0d0460f61489485441160da
kurosutomohiro <kurosu.66@gmail.com>
2022-06-23 23:19:16 +0900

    rubocop指摘修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -12,28 +12,28 @@ def main
   opt.on('-l') { |v| options[:l] = v }
   file_names = opt.parse!(ARGV)
 
-  texts = 
-  if file_names.empty?
-    [{file_name: '', file_contents: $stdin.readlines.join}]
-  else
-    file_names.map do |v|
-      {file_name: v, file_contents: File.read(v)}
+  texts =
+    if file_names.empty?
+      [{ file_name: '', file_contents: $stdin.readlines.join }]
+    else
+      file_names.map do |v|
+        { file_name: v, file_contents: File.read(v) }
+      end
     end
-  end
 
-  calculated_items = calc(file_names, texts, options)
+  calculated_items = calc(texts)
   formatted_items = format(calculated_items)
   display(formatted_items, options)
 end
 
-def calc(file_names, texts, options)
+def calc(texts)
   items =
-    texts.map.with_index do |text, i|
+    texts.map do |text|
       {
         line_count: text[:file_contents].count("\n"),
         word_count: text[:file_contents].split(' ').size,
         byte_count: text[:file_contents].size,
-        file_name: file_names[i]
+        file_name: text[:file_name]
       }
     end
 
@@ -48,17 +48,14 @@ def calc(file_names, texts, options)
     total_byte_count += item[:byte_count]
   end
 
-  totals = {line_count: total_line_count, word_count: total_word_count, byte_count: total_byte_count, file_name: 'total'}
+  totals = { line_count: total_line_count, word_count: total_word_count, byte_count: total_byte_count, file_name: 'total' }
   items.push(totals)
-
-  items
 end
 
 def format(calculated_items)
-  formatted_items =
   calculated_items.map do |calculated_item|
     calculated_item.transform_values do |v|
-      if v.kind_of?(String)
+      if v.is_a?(String)
         v.ljust(ADJUST_SPACE_SIZE)
       else
         v.to_s.rjust(ADJUST_SPACE_SIZE)
@@ -70,7 +67,7 @@ end
 def display(calculated_items, options)
   calculated_items.each do |calculated_item|
     if options[:l]
-      puts "#{calculated_item[:line_count]}#{calculated_item[:file_name]}"
+      puts "#{calculated_item[:line_count]} #{calculated_item[:file_name]}"
     else
       puts "#{calculated_item[:line_count]}#{calculated_item[:word_count]}#{calculated_item[:byte_count]} #{calculated_item[:file_name]}"
     end
